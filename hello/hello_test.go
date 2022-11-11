1dcf502393e285912caa9e4f66211a8ff2c22af9
corin <corinlwilkins@gmail.com>
2022-11-11 08:57:44 +0000

    Initial commit

---
+++ hello/hello_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return  "Hello" + name
+	
+}
+
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
